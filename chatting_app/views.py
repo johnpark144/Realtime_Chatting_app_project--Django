e2724821c40cf8726bed18576b5c2aaa56c0c036
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-08-28 10:32:55 -0500

    Update views.py

--- chatting_app/views.py
+++ chatting_app/views.py
@@ -1,16 +1,13 @@
-
-from email import message
 from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.contrib.auth import authenticate, login 
 from django.contrib.auth.decorators import login_required 
-from django.http import JsonResponse
 from django.utils import timezone
 from .models import Chatting
 from .forms import UserForm
 
 @login_required(login_url='chatting_app:login')
 def chatting_room(request):  
-    messages = Chatting.objects.order_by('create_date') 
+    messages = Chatting.objects.order_by('create_date')     # New message has to come out on very bottom.
     context = {'messages': messages}   
     return render(request, 'chatting.html', context)
 
@@ -18,26 +15,26 @@ def chatting_room(request):
 def send(request):
     messages = Chatting(username = request.user, content=request.POST.get('text'), create_date=timezone.now())
     messages.save()
-    return redirect(resolve_url('chatting_app:chatting_room')+'#last_msg')
+    return redirect(resolve_url('chatting_app:chatting_room')+'#last_msg') # whenever messages sent, last one is anchor to redirect
 
 def signup(request):
-    if request.method == "POST":
+    if request.method == "POST":    # When someone complete the sign-up.
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증 (사용자명과 비밀번호가 정확한지 검증)
-            login(request, user)    # 로그인 시키기
+            user = authenticate(username=username, password=raw_password)  # User authentication (to verify username and password)
+            login(request, user)    # Make them loged in
             return redirect('chatting_app:chatting_room')
-    else:
+    else:   # When someone start the sign-up.
         form = UserForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form}) # Use the form created in signup templete.
 
 @login_required(login_url='common:login')
 def message_delete(request, message_id):
-    message = get_object_or_404(Chatting, pk=message_id)
-    if request.user.username != message.username :
+    message = get_object_or_404(Chatting, pk=message_id)    # To check if the message with its id exist 
+    if request.user.username != message.username :  # To prevent from other user's deleting messages in not proper way.
         return redirect(resolve_url('chatting_app:chatting_room')+'#last_msg')
     message.content = "Deleted message"
     message.save()
